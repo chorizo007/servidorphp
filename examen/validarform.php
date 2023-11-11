75697d5922cf56f04e5a2c4729a3ad9594dee019
chorizo007 <nicolashg2004@gmail.com>
2023-11-11 14:04:04 +0100

    TODO BIEN

--- examen/validarform.php
+++ examen/validarform.php
@@ -1,134 +1,98 @@
 <?php
-    function validarmatricula($file, $matricula){
-        while(!feof($file)){
-            $matruculaaux = substr(fgets($file),0,8);
-            if($matruculaaux==$matricula){
-                return false;
+    include("clases.html");
+    function buscar($file,$matricula,$fechafinal){
+        
+        if($file = fopen($file, 'r+')){
+            while(!feof($file)){
+                $linea= fgets($file);
+                $array_linea = explode(" ", $linea);
+                if($array_linea[0]==$matricula){
+                    if($file == 'basetxt/residentesYHoteles.txt'){
+                        $fecha_de_hoteles = explode("/", $array_linea[3]);
+                        $fecha_de_vehiculos =explode("-", $fechafinal);
+                        $validarfecha = true;
+                        for($ind=0;$ind<3;$ind++){
+                            if((int)$fecha_de_hoteles[$ind]<(int)$fecha_de_vehiculos[$ind]){
+                                return false;
+                            }
+                        }
+                        return true;
+                    }else {
+                        return true;
+                    }
+                }
             }
-            echo $matruculaaux;
+            return false;
+        }else{
+            die("no se puedo acceder a fichero de base de datos");
         }
-        return true;
+        
     }
-    //tipo de ejercicio
+
     if(!empty($_POST['pricipal'])){
-        $validar = true;
-        if($_FILES['certificado']['type']=="application/pdf"){
-            if (is_uploaded_file($_FILES['certificado']['tmp_name'])) {
-                $nombreDirectorio = "./pdfichero/";
-                $nombreFichero = $_FILES['certificado']['name'];
-                $nombreCompleto = $nombreDirectorio . $nombreFichero;
-
-                if (is_dir($nombreDirectorio)) {
-                    $idUnico = time();
-                    $nombreFichero = $idUnico . "-" . $nombreFichero;
-                    $nombreCompleto = $nombreDirectorio . $nombreFichero;
-        
-                    move_uploaded_file($_FILES['certificado']['tmp_name'], $nombreCompleto);
-                    echo "Fichero subido con el nombre: $nombreFichero<br>";
-                } else {
-                    echo 'Directorio definitivo inválido';
-                    $validar=false;
-                }
-            } else {
-                print("No se ha podido subir el fichero\n");
-                $validar=false;
-            }
+        $eleccion = $_POST['opcion'];
+        if($eleccion=="a"){
+            $formulario = "<form action='tipo.php' method='post'>
+            <label>TIPO DE CERTIFICADO:</label>
+            <select name='opcion'>
+                <option value='vehiculosEMT'> vehiculosEMT</option>
+                <option value='taxis'> taxis</option>
+                <option value='servicios'> servicios</option>
+                <option value='residentesYHoteles'> residentesYHoteles</option>
+                <option value='logistica'> logistica</option>
+            </select>
+            <br>
+            <button type='submit' name = 'pricipal' value ='pricipal'>enviar</button>
+            </form>";
+            echo "    <h1>TIPO</h1>
+            <hr>";
+            echo $formulario;
         }
-        else {
-            $validar=false;
-        }
-        
-        if($validar){
-            $matricula = $_POST['matricula'];
-            if(!empty($_POST['calle'])){
-                if($file = fopen('basetxt/vehiculosEMT.txt' , 'r+')){
-                    $respuesta = validarmatricula($file, $matricula);
-                    fclose($file);
-                    if($respuesta){
-                        $calle = $_POST['calle'];
-                        $escribir = fopen('basetxt/vehiculosEMT.txt' , 'a+');
-                        fwrite($escribir,$matricula." ".$calle."\r\n");
-                        fclose($escribir);
-                    }else{
-                        echo "ya esta registrado esa matricula";
+        else{
+            echo "    <h1>INFRACTORES</h1>
+            <a href='pricipal.html'>inicio<a>
+            <hr>";
+            $infractores = 0;
+            $array_infractores = Array();
+            $ruta = Array('basetxt/logistica.txt','basetxt/residentesYHoteles.txt','basetxt/servicios.txt','basetxt/taxis.txt','basetxt/vehiculosEMT.txt');
+            if($file = fopen('basetxt/vehiculos.txt', 'r+')){
+                echo "<table style='border: 1px solid black'> <tr style='border: 1px solid black'> <td>matruculas infractoras</td></tr>";
+                $correcto = true;
+                while(!feof($file)){
+                    $correcto = true;
+                    $linea= fgets($file);
+                    $array_linea = explode(" ", $linea);
+                    if(strcasecmp(trim($array_linea[5]), "electrico") == 0){
+                        $correcto=false;
                     }
-                }else{
-                    die("no se puedo acceder a fichero");
-                }
-            }else if(!empty($_POST['propietario'])){
-                if($file = fopen('basetxt/taxis.txt' , 'r+')){
-                    $respuesta = validarmatricula($file, $matricula);
-                    fclose($file);
-                    if($respuesta){
-                        $propietario = $_POST['propietario'];
-                        $escribir = fopen('basetxt/taxis.txt' , 'a+');
-                        fwrite($escribir,$matricula." ".$propietario."\r\n");
-                        fclose($escribir);
-                    }else{
-                        echo "ya esta registrado esa matricula";
+                    if(array_search($array_linea[0], $array_infractores)!=null){
+                        $correcto=false;   
                     }
-                }else{
-                    die("no se puedo acceder a fichero");
-                }
-            }else if(!empty($_POST['tipo_vehiculo'])){
-                if($file = fopen('basetxt/servicios.txt' , 'r+')){
-                    $respuesta = validarmatricula($file, $matricula);
-                    fclose($file);
-                    if($respuesta){
-                        $tipo_vehiculo = $_POST['tipo_vehiculo'];
-                        $escribir = fopen('basetxt/servicios.txt' , 'a+');
-                        fwrite($escribir,$matricula." ".$tipo_vehiculo."\r\n");
-                        fclose($escribir);
-                    }else{
-                        echo "ya esta registrado esa matricula";
+                    else{
+                        for($ind = 0;$ind<5 && $correcto;$ind++){
+                            if(buscar($ruta[$ind],$array_linea[0],$array_linea[3])){
+                                $correcto=false;
+                            }
+                        }
+                        if($correcto){
+                            echo "<tr> <td style='border: 1px solid black'>".$array_linea[0] . "</td><tr>";
+                            $infractores++;
+                            array_unshift($array_infractores, $array_linea[0]);
+                        }
                     }
-                }else{
-                    die("no se puedo acceder a fichero");
                 }
-            }else if(!empty($_POST['direccion'])){
-                if($file = fopen('basetxt/residentesYHoteles.txt' , 'r+')){
-                    $respuesta = validarmatricula($file, $matricula);
-                    fclose($file);
-                    if($respuesta){
-                        $direccion = $_POST['direccion'];
-                        $fechaInicio = $_POST['fechaInicio'];
-                        $fechafinal = $_POST['fechafinal'];
-
-                        $escribir = fopen('basetxt/residentesYHoteles.txt' , 'a+');
-                        fwrite($escribir,$matricula." ".$direccion. " " . $fechaInicio. " " .$fechafinal."\r\n");
-                        fclose($escribir);
-                    }else{
-                        echo "ya esta registrado esa matricula";
-                    }
-                }else{
-                    die("no se puedo acceder a fichero");
-                }
-            }else if(!empty($_POST['empresa'])){
-                if($file = fopen('../basetxt/logistica.txt' , 'r+')){
-                    $respuesta = validarmatricula($file, $matricula);
-                    fclose($file);
-                    if($respuesta){
-                        $empresa = $_POST['empresa'];
-                        $escribir = fopen('basetxt/logistica.txt' , 'a+');
-                        fwrite($escribir,$matricula." ".$empresa."\r\n");
-                        fclose($escribir);
-                    }else{
-                        echo "ya esta registrado esa matricula";
-                    }
-                }else{
-                    die("no se puedo acceder a fichero");
-                }
-            }
-            else{
-                echo "error 1";
-            }
-        }
-        else {
-            echo "error 2";
+                echo "</table>";
+            }else{
+                die("no se puedo acceder a fichero");
+            } 
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            echo "numero de infractores :" .$infractores;
         }
     }
     else{
-        echo "error 3";
+        echo "error 404";
     }
 
 ?>
