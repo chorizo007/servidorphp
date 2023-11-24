bfac2c7ae75f7bb1ff0c0d7be4ee73fa8a318bd4
nicolas <nicolashg2004@gmail.com>
2023-11-24 09:36:52 +0100

    cookies todo bien
    crear cookies en carpetas

--- cookies/Nivel0/Nivel1/Nivel2/creaCookieNivel.php
+++ cookies/Nivel0/Nivel1/Nivel2/creaCookieNivel.php
@@ -1,11 +1,8 @@
 <?php
-$nombre = $_POST['nombre'] ?? 'por defecto 2';
-$contenido = $_POST['contenido'] ?? 'por defecto 2';
-
-if ($nombre && $contenido) {
-    setcookie($nombre, $contenido, 0, '/');
+$nombre = 'miCookie2';
+$contenido = 'por defecto 2';
+    setcookie($nombre, $contenido, 0);
     echo "Cookie creada con éxito.";
-} else {
-    echo "Todos los campos son obligatorios.";
-}
+
+echo "<br><a href='../../../index.html'>Volver al formulario</a>";
 ?>
