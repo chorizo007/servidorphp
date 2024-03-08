69e26957f426c34045e04dcc988832d035027e5d
chorizo007 <nicolashg2004@gmail.com>
2024-03-08 20:17:49 +0100

    sesio

--- recuperacion-php/principal.php
+++ recuperacion-php/principal.php
@@ -3,10 +3,13 @@ require('nav.php');
 require('constantes.php');
 require('funciones.php');
 
+//SOLO ADMIN
+if (!isset($_SESSION['admin'])) {
+} else {
+    header("Location: login.php");
+}
+
 //A POR TODAS
-
-
-
 
 
 ?>
